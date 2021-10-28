6a9ac283c7544db096ffdbec443c4ecd3fe43ae0
SJoaquinR <rsanjoa@gmail.com>
2021-10-28 09:28:20 -0300

    Se actualiza repo

---
+++ modulo1/filename.js
@@ -0,0 +1,4 @@
+function sayHelloArchivo() {
+    alert("Hello World en archivo")
+    }
+document.write(" Document En archivo")
---
+++ practicaObtenida/ejercicio12/Truck.js
@@ -0,0 +1,7 @@
+class Truck extends Vehicle {
+    constructor( wheels, topSpeed, chargeCapacity ) {
+      super(wheels, topSpeed);
+      this.chargeCapacity = chargeCapacity;
+    }
+  }
+  
